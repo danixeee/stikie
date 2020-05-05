656cbe5cccc14c8e479277a6b2bdccb514c4b57a
Daniel Elero <danixeee@gmail.com>
2020-05-05 20:20:36 +0200

    Refactoring

--- stikiejs/src/comments.js
+++ stikiejs/src/comments.js
@@ -1,13 +1,71 @@
-import { getElementByXPath } from "./utils";
+import {
+  getElementByXPath,
+  uuidv4,
+  createElementFromHTML,
+  getXPathForElement,
+} from "./utils";
 
-class Comment {
+const COMMENTS_CONTAINER_ID = "stikie-comments";
+
+const COMMENT_TEMPLATES = {}; // [template-type] = template-snippet
+
+COMMENT_TEMPLATES["comment1"] = (id) => `
+<div id="${id}" role="comment" class="panel panel-default">
+  <div class="panel-heading">
+    <strong>danixeeee</strong> <span class="text-muted">commented 5 days ago</span>
+    </div>
+    <div class="panel-body">
+    Test test test
+    </div>
+  </div>
+</div>
+`;
+
+COMMENT_TEMPLATES["comment2"] = (id) => `
+<div
+  id=${id} role="comment" class="tooltip">
+  <input type="text" value="Neki input" />
+</div>
+`;
+function getCommentsContainer() {
+  let commentsContainer = document.getElementById(COMMENTS_CONTAINER_ID);
+  if (!commentsContainer) {
+    commentsContainer = createElementFromHTML(`<div id="${COMMENTS_CONTAINER_ID}"></div>`);
+    window.document.body.insertBefore(commentsContainer, window.document.body.firstChild);
+  }
+
+  return commentsContainer
+}
+
+export class Comment {
   constructor(id, path, x, y) {
     this.id = id;
     this.path = path;
     this.x = x;
     this.y = y;
+  }
 
-    this.templates = {}; // [template-type] = template-snippet
+  /*
+    Used when adding comments..
+  */
+  static createAt(targetEl, mouseX, mouseY, type) {
+    const container = getCommentsContainer();
+
+    // create comment as html element
+    const id = uuidv4();
+    const commentEl = createElementFromHTML(Comment.getTemplate(id, type));
+    container.append(commentEl);
+    // calculate relative distance from top-left corner of target element
+    const commentRect = commentEl.getBoundingClientRect();
+    const rect = targetEl.getBoundingClientRect();
+    const x = mouseX - rect.left;
+    const y = mouseY - rect.top + commentRect.height + window.scrollY;
+
+    return new Comment(id, getXPathForElement(targetEl), x, y);
+  }
+
+  static getTemplate(id, type) {
+    return COMMENT_TEMPLATES[type](id);
   }
 
   getPosition() {
@@ -19,27 +77,4 @@ class Comment {
   getHtmlElement() {
     return document.getElementById(this.id);
   }
-
-  static getTemplate(id) {
-    // it should get template from db
-    throw new Error("Not implemented!");
-  }
 }
-
-class Comment extends AbstractComment {
-  static getTemplate(id) {
-    return `
-      <div id="${id}" role="comment" class="panel panel-default">
-        <div class="panel-heading">
-          <strong>myusername</strong> <span class="text-muted">commented 5 days ago</span>
-          </div>
-          <div class="panel-body">
-          Panel content
-          </div>
-        </div>
-      </div>
-    `;
-  }
-}
-
-class StickyNote extends AbstractComment {}
--- stikiejs/src/stikie.js
+++ stikiejs/src/stikie.js
@@ -1,15 +1,9 @@
 import {
-  uuidv4,
-  createElementFromHTML,
-  getXPathForElement,
   isComment,
 } from "./utils";
 
 import { createPopper } from "@popperjs/core";
-
-let STIKIE_COMMENTS_EL;
-
-const commentsDB = [];
+import { Comment } from "./comments"
 
 function create(parent, comment, x = 0, y = 0) {
   createPopper(parent, comment, {
@@ -29,52 +23,26 @@ function create(parent, comment, x = 0, y = 0) {
   });
 }
 
-function addComment(el, e) {
-  const commentsSection = document.getElementById("comments");
-  const id = uuidv4();
-  const commentEl = createElementFromHTML(Comment.getTemplate(id));
-  commentsSection.append(commentEl);
-  const commentRect = document.getElementById(id).getBoundingClientRect();
+function addComment(el, e, type = "comment2" /* will change */) {
+  const comment = Comment.createAt(el, e.clientX, e.clientY, type);
+  const [parent, x, y] = comment.getPosition();
 
-  const rect = el.getBoundingClientRect();
-  const x = e.clientX - rect.left; //x position within the element.
-  const y = e.clientY - rect.top + commentRect.height / 2 + window.scrollY; //y position within the element.
-
-  const comment = new Comment(id, getXPathForElement(el), x, y);
-
-  commentsDB.push(comment);
-
-  const [parent, xx, yy] = comment.getPosition();
-  create(parent, comment.getEl(), xx, yy);
-
-  // renderComments();
-}
-
-function createCommentsContainer() {
-  const stikieCommentsElId = "stikie-comments";
-  const containerEl = createElementFromHTML(
-    `<div id="${stikieCommentsElId}"></div>`
-  );
-
-  window.document.body.insertBefore(
-    containerEl,
-    window.document.body.firstChild
-  );
-
-  return document.getElementById(stikieCommentsElId);
+  // use popper to show comments
+  create(parent, comment.getHtmlElement(), x, y);
 }
 
 /*
+  Allow users to left comments on a site...
  */
 export function register() {
-  STIKIE_COMMENTS_EL = createCommentsContainer();
-
   // do not replace someone's onclick event listener
   const existingOnClickFn = window.onclick;
   window.onclick = (e) => {
+    // execute existing onclick function
     if (existingOnClickFn) {
       existingOnClickFn(e);
     }
+    // add comments
     const elementMouseIsOver = document.elementFromPoint(e.clientX, e.clientY);
     if (!isComment(elementMouseIsOver)) {
       addComment(elementMouseIsOver, e);
